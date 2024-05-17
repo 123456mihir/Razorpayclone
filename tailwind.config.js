d2358583d974fa61571ccf55274c209d11a7347a
MIHIR AGARWAL <107901918+123456mihir@users.noreply.github.com>
2024-05-17 23:07:28 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      mullish:["Mullish","sans-serif"],
+    },
+    colors:{
+      deepBlue:"#02042a",
+      lightBlue:"#2b84ea",
+      LightBlue300:"#4b94ed",
+      LightBlue500:"#0b72e7",
+      greenlight:"#61cea6",
+      grayText:"#818597",
+      lightGray:"#e2e2e2",
+      grayBlue:"#344a6c",
+      deepBlueHead:"#162f56",
+      gray2:"#525a76",
+      white:"#FFFFFF",
+      slate:" #708090 ",
+      gery400:"#808080",
+      grey300:"#E0E0E0",
+      },
+  },
+  plugins: [],
+}
+
